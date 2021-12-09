3f76246bd31786f30c96982266e28112905abcdb
nailtonsimoes <nailtonsimoes@gmail.com>
2021-12-08 22:24:53 -0300

    aula 10

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
-console.log(document.querySelectorAll('.times'))
-console.log(
-  document.getElementById('futebol-europeu').querySelectorAll('.times')[0]
-    .innerHTML
-)
+/* É possivel usar o innerHTML para salvar o conteudo da div na variavel */
+/* 
+var paragrafo = document.getElementById('p1')
+document.getElementById('p1').innerHTML = 'Novo Conteudo'
+paragrafo.innerHTML = 'Novo Conteudo'
+
+paragrafo.style.color = 'red'
+paragrafo.style.backgroundColor = '#333'
+paragrafo.style.height = '200px'
+
+console.log(paragrafo)
+console.log(document.getElementById('p1').innerHTML)
+*/
+
+var imagem = document.getElementById('imagem')
+imagem.src = '7EN_LOGO.jpg'
+imagem.alt = 'Teste de imagem'
+imagem.width = '30'
